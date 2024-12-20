826871a22db62bd93524f250168ebc47e8303ef8
NilufarSalimova2000 <nilufarsalimova2000@gmail.com>
2024-12-20 13:33:16 +0500

    add

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.kun.uz',
+        port: '', 
+      }
+    ]
+    
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <Image 
+      src={'https://storage.kun.uz/source/10/C8LegajuTHiCxALYvB8IEAePl0tVowlJ.jpg'} 
+      quality={1}
+      alt="img" 
+      width={400} 
+      height={400}/>
+    </div>
   );
 }
